639d321112570b903b319ca0a58f930d7611f757
Vandivier, Owen D <ovandivier@yahoo.com>
2022-10-07 13:03:26 -0400

    Created algo.py, usage for slope intersection

---
+++ Python/algo.py
@@ -0,0 +1,31 @@
+
+# y = mx + b
+def get_slope_intersection():
+    y = int(input("Please enter a y coordinate: "))
+    m = int(input("Please enter a slope: "))
+    x = int(input("Please enter a x coordinate: "))
+    
+    res = (m * x) - y
+    print()
+    return "Slope Intersection Result: " + str(res)
+
+
+def main():
+    algo = {
+    "Slope Intersection": get_slope_intersection
+    
+    }
+    
+    # Display Algo list for user input
+    for value in algo:
+        print("List of Algorithms: ", value, end="\n")
+    
+    # Call func, display results to user
+    user_algo = input("Type name of Algorithm to run: ")
+    solution = algo[user_algo]()
+    print(solution)
+    
+    
+
+if __name__ == '__main__':
+    main()
